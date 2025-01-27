20884760cc4d44a9e31a5c0aeb302f3e7454a536
fernandocc1999 <fernandoccorreia1999@gmail.com>
2025-01-27 23:06:47 +0000

    product category added

--- olist_database.sql
+++ olist_database.sql
@@ -76,5 +76,6 @@ CREATE TABLE order_payments (
 
 CREATE TABLE product_category_name_translation (
     product_category_name VARCHAR(255) PRIMARY KEY,
-    product_category_name_english VARCHAR(255)
+    product_category_name_english VARCHAR(255), 
+    product_category_group VARCHAR(255)
 );
